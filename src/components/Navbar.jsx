97e46291fd6b3d452f03ecfad848c56385508c8f
Sad7920 <87930878+Sad7920@users.noreply.github.com>
2025-03-27 20:39:50 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../../public/logo.png'
+
+const Navbar = () => {
+    return (
+        <div className='fixed z-50 w-full h-20 px-4 text-gray-300'>
+            <div className='w-full bg-background/60 backdrop-blur-[10px] h-[60px] max-w-4xl mx-auto p-3 mt-4 flex items-center rounded-full justify-between '>Navbar</div>
+        </div>
+    )
+}
+
+export default Navbar
